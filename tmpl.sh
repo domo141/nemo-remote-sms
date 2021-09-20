f5c1e4b8c9741c4278c9341e68436381a803a379
Tomi Ollila <tomi.ollila@iki.fi>
2021-09-21 01:11:10 +0300

    tmpl.sh: dropped sshpersists and whole subcommand system
    `tmpl.sh sms` is now just `tmpl.sh`.
    
    "host" argument "heuristic" "improved" so that the persistent
    option with 0.A.B.C -like arg will not prefix nemo@ to the
    hostname (which would move ssh persistent connection socket
    usually out-of-reach)
    
    . in addition to '' shown in usage -- also used to choose
    nemo-sms.sh (i.e. local command) to be executed.
    
    dropped -n's (dry-run) from nemo*sms.sh executions.

--- tmpl.sh
+++ tmpl.sh
@@ -45,62 +45,31 @@ x_env () { printf '+ %s\n' "$*" >&2; env "$@"; }
 x_eval () { printf '+ %s\n' "$*" >&2; eval "$*"; }
 x_exec () { printf '+ %s\n' "$*" >&2; exec "$@"; die "exec '$*' failed"; }
 
-test $# -gt 0 || {
-	exec 2>&1
-	printf %s\\n '' "Usage ${0##*/} command ..." '' Commands: ''
-	sed -n 's/^_cmd_\([^ ]*\).*#/  \1  /p' "$0"
-	echo
-	exit
-}
-
-_cmd_sms () #         send sms msg
-{
-	test $# -ge 3 || die "Usage: $0 host|-|'' name message..."
-	case $1 in '-')  r=nemo@$rhost
-		;; ''|.) r= # use nemo-sms.sh
-		;; *@*)  r=$1
-		;; *) r=nemo@$1
-	esac
-	IFS=$NL
-	n=
-	for name in $names
-	do
-		case $name in $2*) n=$name; break; esac
-	done
-	IFS=$saved_IFS
-	test "$n" || die "Cannot find number for '$2'"
-	n=${n#* }
-	shift 2
-	if test "$r"
-	then x_exec $cmdpath''nemo-remote-sms.sh -n "$r" "$n" "$@"
-	else x_exec $cmdpath''nemo-sms.sh -n "$n" "$@"
-	fi
-}
-
-_cmd_sshpersist () #  create ControlPersist -- openssh-compatible...
-{
-	test "$#" -gt 0 || set -- nemo@$rhost exit
-
-	z=`ssh -O check "$@" 2>&1` || {
-		case $z in 'No ControlPath specified'*)
-			echo $z
-			exit 1
-		esac
-		z=${z%)*}; z=${z#*\(}
-		test -e "$z" && rm "$z"
-		x_exec ssh -M -o ControlPersist=3600 "$@"
-		exit not reached
-	}
-	echo $z
-	x_exec ssh "$@"
-	exit not reached
-}
-
-cmd=$1
-shift
-case $cmd in ssh*) cmd=sshpersist
+test $# -ge 3 || die "Usage: $0 [[user]@]host|-|''|. name message..."
+case $1 in '-' ) r=nemo@$rhost
+	;; ''|.) r= # use nemo-sms.sh
+	;; 0.* ) r=$1
+	;; [1-9] | [1-9][0-9] | [1-9][0-9][0-9] ) r=$1
+	;; [1-9][0-9][0-9][0-9] | [1-9][0-9][0-9][0-9][0-9] ) r=$1
+	;; *@* ) r=${1#@} # e.g. @0.1 -> 0.1 ...
+	;; * )   r=nemo@$1 # ... otherwise nemo@
 esac
-_cmd_$cmd "$@"
+
+IFS=$NL
+n=
+for name in $names
+do
+	case $name in $2*) n=$name; break; esac
+done
+IFS=$saved_IFS
+test "$n" || die "Cannot find number for '$2'"
+n=${n#* }
+shift 2
+#x_exec () { echo "$@"; exit; } # outcomment for "dry-run" test
+if test "$r"
+then x_exec $cmdpath''nemo-remote-sms.sh "$r" "$n" "$@"
+else x_exec $cmdpath''nemo-sms.sh "$n" "$@"
+fi
 
 
 # Local variables:
